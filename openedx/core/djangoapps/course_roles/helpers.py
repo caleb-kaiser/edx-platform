f1e99056f38e37fdbe5c236d37072a944d47c37f
hsinkoff <10408711+hsinkoff@users.noreply.github.com>
2023-10-17 19:03:23 +0000

    feat: update format and add to quality matrix

--- openedx/core/djangoapps/course_roles/helpers.py
+++ openedx/core/djangoapps/course_roles/helpers.py
@@ -11,18 +11,20 @@ from xmodule.modulestore.django import modulestore
 # .. toggle_name: FLAG_USE_PERMISSION_CHECKS
 # .. toggle_implementation: WaffleFlag
 # .. toggle_default: False
-# .. toggle_description: Enabling the toggle will allow the db checks for a users permissions. These are used alongside current 
+# .. toggle_description: Enabling the toggle will allow the db checks for a users permissions. These are used alongside current
 #   roles checks. If the flag is not enabled, only the roles checks will be used.
 # .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2023-10-17
 # .. toggle_target_removal_date: 2023-12-01
 USE_PERMISSION_CHECKS_FLAG = WaffleFlag('course_roles.use_permission_checks', module_name=__name__) or False
 
+
 def use_permission_checks():
     """
     Returns ture if permissions checks should be used
     """
     return USE_PERMISSION_CHECKS_FLAG.is_enabled()
+
 
 @request_cached()
 def course_permission_check(user, permission_name, course_id):
