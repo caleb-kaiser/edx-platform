ecceea4ad15185d3501872ce9247bab6030dec16
Caleb Kaiser <caleb.kaiser.js@gmail.com>
2023-10-23 09:37:16 -0400

    tweak

--- lms/djangoapps/registration_proxy/views.py
+++ lms/djangoapps/registration_proxy/views.py
@@ -1,15 +1,19 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.test.utils import override_settings
+import logging
 import requests
 
-@override_settings(DEBUG=True)
+logger = logging.getLogger(__name__)
+
 @csrf_exempt
 def registration_proxy(request):
     if request.method == "POST":
+        post_data = json.loads(request.body.decode('utf-8'))
+        logger.info("POST data: %s", post_data)
+
         external_api_url = "https://www.comet.com/api/auth/new"
 
-        response = requests.post(external_api_url, data=request.POST)
+        response = requests.post(external_api_url, data=post_data)
 
         # Forward the status code and content from the external service to your client
         return JsonResponse(response.json(), status=response.status_code)
