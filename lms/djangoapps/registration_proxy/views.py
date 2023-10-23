932f4ce99cd01528c65504a0ff5b99d7839da09e
Caleb Kaiser <caleb.kaiser.js@gmail.com>
2023-10-23 11:26:06 -0400

    Fix media type

--- lms/djangoapps/registration_proxy/views.py
+++ lms/djangoapps/registration_proxy/views.py
@@ -12,9 +12,14 @@ def registration_proxy(request):
         post_data = json.loads(request.body.decode('utf-8'))
         logger.info("POST data: %s", post_data)
 
+        headers = {
+            'Content-type':'application/json', 
+            'Accept':'application/json'
+        }
+
         external_api_url = "https://www.comet.com/api/auth/new"
 
-        response = requests.post(external_api_url, data=post_data)
+        response = requests.post(external_api_url, json=post_data, headers=headers)
 
         # Forward the status code and content from the external service to your client
         return JsonResponse(response.json(), status=response.status_code)
